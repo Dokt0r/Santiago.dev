85d9d640325ae3c3621179b7fc3022e405dc699c
Dokt0r <ssante@ucm.es>
2026-09-02 19:09:34 +0200

    Estructura inicial

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section className="header">
+      </section>
+      <section className="hero"></section>
+    </>
+  )
+}
+
+export default App
